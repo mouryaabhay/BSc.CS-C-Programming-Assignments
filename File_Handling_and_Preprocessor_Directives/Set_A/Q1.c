be23ca37ff7d2321d8c71836ca4809381428a124
Mourya Abhay Amarjeet <mouryaabhay2426@gmail.com>
2024-03-18 18:09:07 +0530

    Add answer of question 1 of Set B.

--- File_Handling_and_Preprocessor_Directives/Set_A/Q1.c
+++ File_Handling_and_Preprocessor_Directives/Set_A/Q1.c
@@ -1,2 +1,19 @@
 // Define a macro PI and CIRCLE_AREA(r) which gives the area of circle.
 // Use these macros in main.
+
+#include <stdio.h>
+#define PI 3.142
+#define CIRCLE_AREA(r) (PI * r * r)
+
+int main() {
+    float radius, area;
+
+    printf("Enter the radius of the circle: ");
+    scanf("%f", &radius);
+
+    area = CIRCLE_AREA(radius); // Using the macro to calculate the area
+
+    printf("The area of the circle is: %.4f\n", area);
+
+    return 0;
+}
--- File_Handling_and_Preprocessor_Directives/Set_B/Q2.c
+++ File_Handling_and_Preprocessor_Directives/Set_B/Q2.c
@@ -1,2 +1,30 @@
 // A file “numbers.txt” contains a set of integers.
 // Write a program to read this file and copy all even numbers to file “even.txt” and odd numbers to file “odd.txt”.
+
+#include <stdio.h>
+
+int main() {
+    FILE *numbers = fopen("numbers.txt", "r");
+    FILE *even = fopen("even.txt", "w+");
+    FILE *odd = fopen("odd.txt", "w+");
+
+    if (numbers == NULL || even == NULL || odd == NULL) {
+        printf("Error opening files.\n");
+        return 1;
+    }
+
+    int num;
+    while (fscanf(numbers, "%d", &num) != EOF) {
+        if (num % 2 == 0) {
+            fprintf(even, "%d\n", num);
+        } else {
+            fprintf(odd, "%d\n", num);
+        }
+    }
+
+    fclose(numbers);
+    fclose(even);
+    fclose(odd);
+
+    return 0;
+}
