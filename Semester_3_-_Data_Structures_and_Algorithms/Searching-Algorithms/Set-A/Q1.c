170dde5fa20d2be3b51384a04c107091076cf160
Mourya Abhay Amarjeet <mouryaabhay2426@gmail.com>
2024-08-02 18:55:34 +0530

    Assignment 1 - Set A answers

--- Semester_3_-_Data_Structures_and_Algorithms/Searching-Algorithms/Set-A/Q1.c
+++ Semester_3_-_Data_Structures_and_Algorithms/Searching-Algorithms/Set-A/Q1.c
@@ -1,1 +1,70 @@
 // Create a random array of n integers. Accept a value x from user and write a function to implement linear search algorithm to check whether the number is present in the array or not and output the position if the number is present.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Function to generate a random array of n integers
+void generate_random_array(int arr[], int n) {
+    int i;
+    for (i = 0; i < n; i++) {
+        arr[i] = rand() % 100; // Generate random numbers between 0 and 99
+    }
+}
+
+// Function to perform linear search
+int linear_search(int arr[], int n, int key) {
+    int i;
+    for (i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            return i; // Return the position of the element if found
+        }
+    }
+    return -1; // Return -1 if the element is not found
+}
+
+// Function to accept array of integers from user
+void accept(int arr[], int n) {
+    int i;
+    for (i = 0; i < n; i++) {
+        scanf ("%d", &arr[i]);
+    }
+}
+
+// Function to display array of integers
+void display(int arr[], int n) {
+    int i;
+    for (i = 0; i < n; i++) {
+        printf ("%d ", arr[i]);
+    }
+}
+
+int main() {
+
+    int n, x, i, result;
+    printf ("Enter the size of the array: ");
+    scanf ("%d", &n);
+
+    // Generating random array automatically
+    int arr[n];
+    generate_random_array(arr, n);
+
+    // Accepting Array from the user
+    // printf ("Enter the %d elements of the array:\n", n);
+    // accept(arr, n);
+
+    printf ("Random array generated:\n");
+    display(arr, n);
+
+    printf ("\nEnter the number to search: ");
+    scanf ("%d", &x);
+
+    result = linear_search(arr, n, x);
+
+    if (result != -1) {
+        printf ("Number %d found at position %d.\n", x, result + 1);
+    } else {
+        printf ("Number %d not found in the array.\n", x);
+    }
+
+    return 0;
+}
--- Semester_3_-_Data_Structures_and_Algorithms/Searching-Algorithms/Set-A/Q2.c
+++ Semester_3_-_Data_Structures_and_Algorithms/Searching-Algorithms/Set-A/Q2.c
@@ -1,1 +1,51 @@
 // Accept n sorted values in array from user. Accept a value x from user and write a function to implement binary search algorithm to check whether the number is present in sorted array or not and output the position if the number is present.
+
+#include <stdio.h>
+
+// Function to perform binary search
+int binarySearch(int arr[], int n, int key) {
+    int begin = 0, end = n - 1, mid;
+
+    while (begin <= end) {
+        mid = (begin + end) / 2;
+
+        // Check if key is present at mid
+        if (arr[mid] == key)
+            return mid;
+
+        // If key greater, ignore left half
+        // If key is smaller, ignore right half
+        if (arr[mid] < key)
+            begin = mid + 1;
+        else
+            end = mid - 1;
+    }
+
+    return -1; // Element not present
+}
+
+int main() {
+    int n, x, i, result;
+
+    printf ("Enter the number of elements in the array: ");
+    scanf ("%d", &n);
+
+    int arr[n];
+
+    printf ("Enter %d sorted elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf ("%d", &arr[i]);
+    }
+    
+    printf ("Enter the value to search: ");
+    scanf ("%d", &x);
+
+    result = binarySearch(arr, n, x);
+
+    if (result == -1)
+        printf ("Element is not present in array\n");
+    else
+        printf ("Element is present at position %d\n", result + 1);
+
+    return 0;
+}
