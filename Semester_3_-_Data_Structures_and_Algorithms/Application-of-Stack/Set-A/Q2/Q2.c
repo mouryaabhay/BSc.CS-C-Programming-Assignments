680947eddc5cbffb09eef8cb7826b0491d14d2bc
Mourya Abhay Amarjeet <mouryaabhay2426@gmail.com>
2024-10-04 18:40:39 +0530

    Application of Stack answers added

--- Semester_3_-_Data_Structures_and_Algorithms/Application-of-Stack/Set-A/Q2/Q2.c
+++ Semester_3_-_Data_Structures_and_Algorithms/Application-of-Stack/Set-A/Q2/Q2.c
@@ -1,13 +1,12 @@
 // Write a program to convert an infix expression to its equivalent postfix notation. Consider usual precedence's of operators. Use stack library of stack of characters using static implementation.
 
 #include <stdio.h>
-#include <string.h>
 #include "intopost.h"
 
 void main() {
-    char infix[MAX_SIZE], postfix[MAX_SIZE];
-    printf("Enter an infix expression: ");
-    gets(infix);
+    char infix[MAXSIZE], postfix[MAXSIZE];
+    printf("\nEnter an infix expression: ");
+    scanf("%s", infix);
     infixToPostfix(infix, postfix);
-    printf("Postfix expression: %s\n", postfix);
+    printf("Postfix expression: %s\n\n", postfix);
 }
